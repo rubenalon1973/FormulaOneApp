5cab5b6412360a58e9127d4a5115dedf92a14d20
rubenalon1973 <rubenalonso1973@gmail.com>
2023-12-12 19:37:13 +0100

    Initial Commit

---
+++ FormulaOneApp/FormulaOneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaOneAppApp.swift
+//  FormulaOneApp
+//
+//  Created by Ruben Alonso on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FormulaOneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
