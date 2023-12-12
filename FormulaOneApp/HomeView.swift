c50cb78c0f96f1e4139e4a6fef3ecb3036234a9a
rubenalon1973 <rubenalonso1973@gmail.com>
2023-12-12 20:35:19 +0100

    Creating all folders of the previously planned project

--- FormulaOneApp/HomeView.swift
+++ FormulaOneApp/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  FormulaOneApp
 //
 //  Created by Ruben Alonso on 12/12/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    HomeView()
 }
