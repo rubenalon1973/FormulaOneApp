48a97696f0e2da45b9d524bfb96651532ff79795
rubenalon1973 <rubenalonso1973@gmail.com>
2023-12-27 20:14:20 +0100

    Cleaning

--- FormulaOneApp/HomeView.swift
+++ FormulaOneApp/HomeView.swift
@@ -13,6 +13,7 @@ enum Tab {
     case drivers
     case teams
     case settings
+    case history
     
     var title: String {
         switch self {
@@ -26,89 +27,12 @@ enum Tab {
                 return "Teams"
             case .settings:
                 return "Settings"
+            case .history:
+                return "History"
         }
     }
 }
 
-//struct HomeView: View {
-//    @State var selection = Tab.nextRaces
-//    @ObservedObject var currentRaceVM = CurrentRaceTableVM()
-//    @ObservedObject var raceResultVM = RaceResultsVM()
-//    @ObservedObject var currentDriverVM = CurrentDriverInfoVM()
-//    @ObservedObject var settingsVM = SettingsVM()
-//    @ObservedObject var currentConstructorVM = CurrentConstructorInfoVM()
-//    @ObservedObject var allSeasonVM = AllSeasonTableVM()
-//
-//    var body: some View {
-//        NavigationStack {
-//            TabView(selection: $selection) {
-//                NextRacesView(currentVM: currentRaceVM)
-//                    .tabItem {
-//                        VStack {
-//                            Text("Next races")
-//                            Image(systemName: "flag.checkered.2.crossed")
-//                        }
-//                    }
-//                    .tag(Tab.nextRaces)
-//                    .navigationBarHidden(selection != .nextRaces)
-//                RaceResultView(raceVM: raceResultVM)
-//                    .tabItem {
-//                        VStack {
-//                            Text("Results")
-//                            Image(systemName: "list.bullet.clipboard")
-//                        }
-//                    }
-//                    .tag(Tab.results)
-//                    .navigationBarHidden(selection != .results)
-//                DriverInfoView(driverVM: currentDriverVM)
-//                    .tabItem {
-//                        VStack {
-//                            Text("Drivers")
-//                            Image(systemName: "person.fill")
-//                        }
-//                    }
-//                    .tag(Tab.drivers)
-//                    .navigationBarHidden(selection != .drivers)
-//                ConstructorInfoView(constructorVM: currentConstructorVM)
-//                    .tabItem {
-//                        VStack {
-//                            Text("Teams")
-//                            Image(systemName: "car.2")
-//                        }
-//                    }
-//                    .tag(Tab.teams)
-//                    .navigationBarHidden(selection != .teams)
-//                SettingsView(settingsVM: settingsVM)
-//                    .tabItem {
-//                        VStack {
-//                            Text("Settings")
-//                            Image(systemName: "gear")
-//                        }
-//                    }
-//                    .tag(Tab.settings)
-//                    .navigationBarHidden(selection != .settings)
-//            }
-//            .navigationTitle(selection.title)
-//            .navigationBarTitleDisplayMode(.inline)
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    NavigationLink {
-//                        AllSeasonTableListView(allSeasonVM: allSeasonVM)
-//                    } label: {
-//                        VStack {
-//                            Image(systemName: "flag.filled.and.flag.crossed")
-//                                .foregroundColor(Color.blue)
-//                            Text("History")
-//                                .font(.caption)
-//                                .bold()
-//                                .foregroundColor(Color.blue)
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
 struct HomeView: View {
     @State var selection = Tab.nextRaces
     @ObservedObject var currentRaceVM = CurrentRaceTableVM()
